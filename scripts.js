22ad963208cd55789757611dd4add2328bac7424
GeoGri <grzegorz1wojda@gmail.com>
2017-06-20 10:32:00 +0200

    for work

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 	// Animating progress bar
-	var keys = [100, 50, 30, 20];
+	var keys = [100, 80, 50, 30, 20];
 	var progressBars = {
 					100: '#bar-windows, #bar-pl',
-					 50: '#bar-html, #bar-git',
-					 30: '#bar-css, #bar-rails',
-					 20: '#bar-en, #bar-bootstrap, #bar-javascript, #bar-jq, #bar-linux'};
+					 80: '#bar-html',
+					 50: '#bar-git, #bar-css',
+					 30: '#bar-en, #bar-rails, #bar-linux',
+					 20: '#bar-bootstrap, #bar-javascript, #bar-jq, #bar-adobe, #bar-sql'};
 
-	for (var i = 0; i <= 3; i++) {
+	for (var i = 0; i <= 4; i++) {
 		$(progressBars[keys[i]]).stop().animate({
 			width: keys[i].toString() + '%'
 		});
